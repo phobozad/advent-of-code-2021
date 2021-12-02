f51ba5835e6cbdd6feb7c2c30fbb2de193e26f54
Chris Burger <chris@boop.li>
2021-12-02 16:55:55 -0600

    Day 2 Complete

---
+++ Day2/day2-partdeux.py
@@ -0,0 +1,34 @@
+
+
+with open("day2-input.txt","r") as file:
+    data = file.read().splitlines()
+
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for command in data:
+
+    cmdArray = command.split()
+    cmdAction = cmdArray[0]
+    cmdValue = int(cmdArray[1])
+
+    # directionVector will either be 1 or -1 depending on how the value should be adjusted
+    if "up" in cmdAction:
+        # "up" decreases depth, so use -1 for our vector
+        directionVector = -1
+    else:
+        # Everything else adds to the current value
+        directionVector = 1
+
+    if "up" in cmdAction or "down" in cmdAction:
+        # up/down affects aim
+        aim = aim + cmdValue*directionVector
+    else:
+        # only other command option is "forward", which affects horizontal position & depth
+        horizontal = horizontal + cmdValue*directionVector
+        depth = depth + aim*cmdValue*directionVector
+
+
+print(f"Horizontal: {horizontal} | Depth: {depth} | MultipliedValue: {horizontal*depth}")
